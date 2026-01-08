3fc5c0bee8a6a36282e60adb5022808093012fb8
Trung Nghia Dev <tr.trungnghia2001@gmail.com>
2026-01-08 22:15:17 +0700

    update UI and api ophim

--- client/src/pages/MangaDetailPage.tsx
+++ client/src/pages/MangaDetailPage.tsx
@@ -2,6 +2,7 @@ import { truyenBySlug, truyenImage } from "@/services/otruyen";
 import { useQuery } from "@tanstack/react-query";
 import { useMemo } from "react";
 import { Link, useParams } from "react-router-dom";
+import book from "@/assets/icon/book.svg";
 
 const MangaDetailPage = () => {
   const { slug } = useParams();
@@ -16,17 +17,17 @@ const MangaDetailPage = () => {
   }, [info]);
 
   return (
-    <div className="max-w-7xl w-full mx-auto">
-      <section className="flex items-start gap-8 max-w-4/5">
+    <div className="max-w-7xl w-full mx-auto space-y-14 px-4">
+      <section className="flex flex-col md:flex-row items-center md:items-start gap-8">
         <img
           src={truyenImage(mangaInfo?.thumb_url)}
           alt="thumb"
           loading="lazy"
-          className="rounded-lg overflow-hidden w-3xs"
+          className="rounded-lg overflow-hidden w-3xs aspect-thumbnail"
         />
         <div className="flex-1 space-y-2">
           <h3>{mangaInfo?.name}</h3>
-          <p className="text-muted-foreground flex flex-wrap gap-2">
+          <div className="text-muted-foreground flex flex-wrap gap-2">
             <h5>Thể loại: </h5>
             {mangaInfo?.category.map((cate) => (
               <Link
@@ -37,10 +38,7 @@ const MangaDetailPage = () => {
                 #{cate.name}
               </Link>
             ))}
-          </p>
-          <p className="text-muted-foreground">
-            <h5>Tags: </h5>
-          </p>
+          </div>
           <h5 className="text-muted-foreground">Thông tin:</h5>
           <ul className="grid grid-cols-2 gap-1 text-muted-foreground">
             <li>Tình trạng: {mangaInfo?.status}</li>
@@ -56,7 +54,30 @@ const MangaDetailPage = () => {
           ></p>
         </div>
       </section>
-      <section></section>
+      <section className="space-y-4">
+        <h3>Danh sách chương</h3>
+        <div className="border-t border-border"></div>
+        <ul className="max-h-[580px] overflow-y-auto">
+          {mangaInfo?.chapters[0].server_data.map((chapter) => (
+            <li
+              key={chapter.chapter_name}
+              className="border-b border-b-gray-500 last:border-none hover:bg-gray-800"
+            >
+              <Link
+                to={
+                  `/manga/${slug}/chapter/` +
+                  chapter.chapter_api_data.split("/").pop()
+                }
+                className="flex items-center gap-2 py-2"
+              >
+                <img src={book} alt="icon" loading="lazy" className="w-4" />
+                <span>Chapter {chapter.chapter_name}</span>
+                <span>{chapter.chapter_title}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 };
